2ad03858c805ba6bdad6580db22cbddcde3ba4d0
V-woodpecker-V <johnnykgen@gmail.com>
2026-01-26 07:35:24 +0100

    progress

--- warg/warg/model.go
+++ warg/warg/model.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 )
 
-type FlagFactory[T any] func() FlagDef[T]
-type rootFlag struct{}
+var errTypeMismatch = fmt.Errorf("type mismatch")
 
-type Setter[T any] interface {
-	Set(T)
+type FlagFactory func() *FlagDef
+type FlagOpt func(*FlagDef)
+
+type Setter interface {
+	Set(any) error
 }
 
 type flagSetter[T any] struct {
@@ -22,8 +24,12 @@ func PointsTo[T any](ptr *T) flagSetter[T] {
 	}
 }
 
-func (s flagSetter[T]) Set(val T) {
-	*s.ptr = val
+func (s flagSetter[T]) Set(val any) error {
+	if v, ok := val.(T); ok {
+		*s.ptr = v
+		return nil
+	}
+	return errTypeMismatch
 }
 
 type fieldSetter[T any] struct {
@@ -36,19 +42,23 @@ func TakesFrom[T any](acc func() *T) fieldSetter[T] {
 	}
 }
 
-func (s fieldSetter[T]) Set(val T) {
-	*s.acc() = val
+func (s fieldSetter[T]) Set(val any) error {
+	if v, ok := val.(T); ok {
+		*s.acc() = v
+		return nil
+	}
+	return errTypeMismatch
 }
 
 type CommandFunc func(args any) error
 
-type FlagDef[T any] struct {
-	Names       []string       `json:"names"`
-	Description string         `json:"description"`
-	Children    []FlagDef[any] `json:"children"`
-	Command     CommandFunc    `json:"-"`
-	IsSet       bool           `json:"-"`
-	Setter      Setter[T]      `json:"-"`
+type FlagDef struct {
+	Names       []string    `json:"names"`
+	Description string      `json:"description"`
+	Children    []*FlagDef  `json:"children"`
+	Command     CommandFunc `json:"-"`
+	IsSet       bool        `json:"-"`
+	Setter      Setter      `json:"-"`
 
 	isRoot bool
 }
@@ -66,75 +76,68 @@ type TestObject struct {
 func T() {
 	var t TestStruct
 
-	flags := NewParser("Some general help",
-		WithFlag([]string{"-b", "--bool"}, "this is a bool", PointsTo(&t.TestBool)),
-		WithFlag([]string{"-i", "--integer"}, "this is an integer", PointsTo(&t.TestInt)),
-		WithContext([]string{"-o", "--object"}, "this is an object", PointsTo(&t.TestObj)),
+	flags := Define("Some general help",
+		Flag([]string{"-b", "--bool"}, "this is a bool", PointsTo(&t.TestBool)),
+		Flag([]string{"-i", "--integer"}, "this is an integer", PointsTo(&t.TestInt)),
+		Context([]string{"-o", "--object"}, "this is an object", PointsTo(&t.TestObj),
+			Flag([]string{"-s", "--string"}, "this is a string", TakesFrom(func() *T {}))), // TODO: THis bitch
 	)
 
 	j, _ := json.MarshalIndent(flags, "", "  ")
 	fmt.Printf("%v\n", string(j))
 }
 
-func NewParser(description string, defs ...FlagFactory[any]) error {
-	WithContext([]string{}, description, PointsTo(&struct{}{}), defs...)
+func Define(description string, defs ...FlagFactory) error {
+	Context([]string{}, description, PointsTo(&struct{}{}), defs...)
 	return nil
 }
 
-func WithFlag[T any](names []string, description string, setter flagSetter[T]) FlagFactory[T] {
-	return func() FlagDef[T] {
-		return FlagDef[T]{
+func Flag(names []string, description string, setter Setter, opts ...FlagOpt) FlagFactory {
+	return func() *FlagDef {
+		f := &FlagDef{
 			Names:       names,
 			Description: description,
-			Children:    []FlagDef[any]{},
+			Children:    []*FlagDef{},
 			Command:     nil,
 			Setter:      setter,
 		}
+		for _, opt := range opts {
+			opt(f)
+		}
+		return f
 	}
 }
 
-func WithContext[T any](names []string, description string, setter Setter[T], children ...FlagFactory[any]) FlagFactory[T] {
-	return func() FlagDef[T] {
-		childDefs := []FlagDef[any]{}
-		for _, def := range children {
+func Context(names []string, description string, setter Setter, children ...FlagFactory) FlagFactory {
+	return Flag(
+		names,
+		description,
+		setter,
+		WithSubFlags(children...))
+}
+
+func SubCommand(names []string, description string, setter Setter, cmd CommandFunc, children ...FlagFactory) FlagFactory {
+	return Flag(
+		names,
+		description,
+		setter,
+		WithCommand(cmd),
+		WithSubFlags(children...),
+	)
+}
+
+func WithSubFlags(flags ...FlagFactory) FlagOpt {
+	return func(f *FlagDef) {
+		childDefs := []*FlagDef{}
+		for _, def := range flags {
 			childDefs = append(childDefs, def())
 		}
-
-		return FlagDef[T]{
-			Names:       names,
-			Description: description,
-			Children:    childDefs,
-			Command:     nil,
-			Setter:      setter,
-		}
+		f.Children = childDefs
 	}
 }
 
-func WithSubCommand[T any](names []string, description string, setter Setter[T], cmd CommandFunc, children ...FlagFactory[any]) FlagFactory[T] {
-	return func() FlagDef[T] {
-		childDefs := []FlagDef[any]{}
-		for _, def := range children {
-			childDefs = append(childDefs, def())
-		}
-
-		return FlagDef[T]{
-			Names:       names,
-			Description: description,
-			Children:    childDefs,
-			Command:     cmd,
-			Setter:      setter,
-		}
-	}
-}
-
-func WithField[T any](names []string, description string, setter Setter[T]) FlagFactory[T] {
-	return func() FlagDef[T] {
-		return FlagDef[T]{
-			Names:       names,
-			Description: description,
-			Children:    []FlagDef[any]{},
-			Command:     nil,
-			Setter:      setter,
-		}
+func WithCommand(cmd CommandFunc) FlagOpt {
+	return func(f *FlagDef) {
+		f.Command = cmd
 	}
 }
