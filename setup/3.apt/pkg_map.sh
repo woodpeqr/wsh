237b8aa0da89ac2929b3fe8b8ca7d6e87effc53f
V-Woodpecker-V <johnnykgen@gmail.com>
2026-03-19 11:15:21 +0100

    remove zellij from apt install

--- setup/3.apt/pkg_map.sh
+++ setup/3.apt/pkg_map.sh
@@ -3,5 +3,6 @@ case "$1" in
     fd)                      echo "fd-find" ;;
     zsh-autosuggestions)     echo "zsh-autosuggestions" ;;
     zsh-syntax-highlighting) echo "zsh-syntax-highlighting" ;;
+    zellij)                  echo "" ;;
     *)                       echo "$1" ;;
 esac
